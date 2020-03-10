d36827eff28a106818fb620a91eeb42e465cf324
Andrew Duchaussee <54364109+ADuchaussee@users.noreply.github.com>
2020-03-10 19:50:51 -0400

    Add files via upload
    All done

---
+++ Calc/Calc/scripts/operations.js
@@ -0,0 +1,35 @@
+
+
+let list = [...document.querySelectorAll(".Number"), ... document.querySelectorAll(".operations")];
+for(e of list){
+    e.addEventListener('click', function(event)
+    {
+        document.querySelector("#input").value += this.value;
+    });
+};
+
+const doc = document.querySelector("#input");
+
+function Clear() {
+    document.querySelector("#input").value = '';
+}
+
+function Backspace(){
+    
+    doc.value = doc.value.substring(0, doc.value.length - 1);
+}
+
+function EvalThis()
+{
+    if(doc.value[0]==0 && Number.isInteger(doc.value[1])){
+        doc.value = " You can lead with 0 you muppet"
+    }
+    else{
+        try{
+  doc.value = eval(doc.value);
+        }
+        catch(e){
+            doc.value = "Doesn't work like that"
+        }
+    }
+}
